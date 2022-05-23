f6815a1c29043c166c3e34246a1c3d08969efb48
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-05-23 18:29:28 -0300

    error handling

--- main.js
+++ main.js
@@ -9,11 +9,15 @@ $('#btn').on('click', function() {
 
 function getData() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=PVdtYcjSTuenRt1s44hdW25oRDZLfO87FsFdmMq9&date=${newDate}`)
-        .then(res => res.json())
-        .then(data => {
-            img.src = data.hdurl;
-            info.textContent = data.explanation;
-            title.textContent = data.title;
+    .then(res => {
+        if(res.ok) {
+            res.json().then(data => {
+                img.src = data.hdurl;
+                info.textContent = data.explanation;
+                title.textContent = data.title;
+                error.style.visibility = 'hidden';
+            });
+        } else (error.style.visibility = 'visible');
     });
 };
 
