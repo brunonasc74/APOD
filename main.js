36e44972c089cc94618f9a1fa331a3b8ab745416
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-05-25 17:57:59 -0300

    surprise me button added

--- main.js
+++ main.js
@@ -7,17 +7,30 @@ $('#btn').on('click', function() {
     return getData();
 });
 
+$('#count').on('click', () => getRandomData());
+
 function getData() {
     fetch(`https://api.nasa.gov/planetary/apod?api_key=PVdtYcjSTuenRt1s44hdW25oRDZLfO87FsFdmMq9&date=${newDate}`)
     .then(res => {
         if(res.ok) {
             res.json().then(data => {
-                img.src = data.hdurl;
+                img.src = data.hdurl ?? data.url;
                 info.textContent = data.explanation;
                 title.textContent = data.title;
+                date.value = data.date;
                 error.style.visibility = 'hidden';
             });
         } else (error.style.visibility = 'visible');
     });
 };
 
+function getRandomData() {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=PVdtYcjSTuenRt1s44hdW25oRDZLfO87FsFdmMq9&count=1`)
+        .then(res => res.json())
+        .then(data => {
+            img.src = data[0].hdurl ?? data[0].url;
+            info.textContent = data[0].explanation;
+            title.textContent = data[0].title;
+            date.value = data[0].date;
+        });
+}
